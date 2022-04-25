b25311ca589bdec8f034f77721e08b7cd15631fc
Adadezer <adadezer@gmail.com>
2022-04-25 11:50:01 -0300

    req 07, 09, 11, 13

--- app/backend/src/middlewares/loginMiddleware.ts
+++ app/backend/src/middlewares/loginMiddleware.ts
@@ -4,23 +4,19 @@ export default function validationLogin(req: Request, res: Response, next: NextF
   const { email, password } = req.body;
 
   if (!email) {
-    return res.status(400).json({ message: 'email is required' });
+    return res.status(400).json({ message: 'All fields must be filled' });
   }
 
   if (!password) {
-    return res.status(400).json({ message: 'password is required' });
+    return res.status(400).json({ message: 'All fields must be filled' });
   }
 
-  if (typeof email !== 'string' || typeof password !== 'string') {
-    return res.status(400).json({ message: 'email and password must be a string' });
+  if (!email.includes('@') || !email.endsWith('.com') || typeof email !== 'string') {
+    return res.status(401).json({ message: 'Incorrect email or password' });
   }
 
-  if (!email.includes('@') || !email.endsWith('.com')) {
-    return res.status(400).json({ message: 'email is incorrect ' });
-  }
-
-  if (password.length <= 6) {
-    return res.status(400).json({ message: 'password must be longer than 6 characters' });
+  if (password.length <= 6 || typeof password !== 'string') {
+    return res.status(400).json({ message: 'Incorrect email or password' });
   }
   next();
 }
